aed663ba11b57f0eb5624cb8f5467f178f7f3204
Guilherme <guifmoraiis@gmail.com>
2026-06-10 18:21:31 -0300

    adiciona estrutura inicial do backend - spring boot

---
+++ backend/src/test/java/com/github/guifelipem/GuifelipemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.guifelipem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuifelipemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
