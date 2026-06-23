ba6a56fdfb260c682978bcef390c18e4144f5c50
Guilherme <guifmoraiis@gmail.com>
2026-06-23 16:33:40 -0300

    feature: implementar estrura inicial do frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-background text-foreground">
+      <Button>Entrar no Help Desk</Button>
+    </main>
+  );
+}
+
+export default App;
